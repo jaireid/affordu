9ef7e8239a80dfa4421592ac0b5fff33ce2d427b
Jai Reid <jaiwilliamsreid@gmail.com>
2023-04-10 02:22:12 +0000

    Feat: Firebase Configuration

---
+++ src/utils/firebase-config.tsx
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID
+};
+
+export function getFirebaseConfig() {
+  if(!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error("No firebase configuration");
+  } else {
+    return firebaseConfig;
+  }
+}
---
+++ src/utils/firebase.tsx
@@ -0,0 +1,13 @@
+// Import the functions from SDKs
+import { getFirebaseConfig } from "./firebase-config";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Initialize Firebase
+const app = initializeApp(getFirebaseConfig());
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
