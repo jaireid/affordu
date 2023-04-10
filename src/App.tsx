44acc71575c81bec1483b85f73ae1653fbeb9551
Jai Reid <jaiwilliamsreid@gmail.com>
2023-04-10 05:58:52 +0000

    Feat: Setup routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css'
+import { useContext, useEffect } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import "./App.css";
 
 export default function App() {
+    const { currentUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    // Check if currentUser exists on initial render
+    useEffect(() => {
+        if (currentUser) {
+            navigate("/profile");
+        }
+    }, [currentUser]);
+  
     return (
-        <main>
-          
-        </main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route 
+            path="profile" 
+            element={
+              <ProtectedRoute>
+                  <Profile />
+              </ProtectedRoute>
+            }
+          />
+      </Routes>
     )
-}
+};
