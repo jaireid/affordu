e97cb68906bbd165c857d48b14e836060f1a90a2
Jai Reid <jaiwilliamsreid@gmail.com>
2023-04-10 05:36:28 +0000

    Bug: Improper import of AuthContext and typo in return statement

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { AuthProvider } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Profile() {
     const { currentUser, signOut } = useContext(AuthContext);
 
     return (
         <>
-            <h2>Welcome, {user.displayName}!</h2>
+            <h2>Welcome, {currentUser?.displayName}!</h2>
             <p>This is your home page.</p>
             <button onClick={signOut}>Sign Out</button>
         </>
