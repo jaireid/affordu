ac6e56be50567d3afc2b87586f7b6d19f68a4e15
jaireid <jaiwilliamsreid@gmail.com>
2023-05-02 07:26:36 +0000

    Feat: Navigation buttons
    - Navigation buttons link to profile page
    - Navigation buttons change content displayed when clicked
    - Add component that will display colleges saved by the user

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,52 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useLocation } from "react-router-dom";
 import { fetchColleges } from "../api/collegeAPI";
 import Search from "./Search";
+import SavedColleges from "./SavedColleges"
 import { Container, Title, Text } from "@mantine/core";
 
 export default function Profile() {
     const { currentUser } = useContext(AuthContext);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const type = searchParams.get("type");
+
+    let content;
+    const search = <>
+                        <Title order={2} mb="md" ta="center">
+                            Find your dream college without breaking the bank
+                        </Title>
+                        <Container size="md" px="lg">
+                            <Text 
+                                color="dimmed" 
+                                size="sm" 
+                                pb="md" 
+                                ta={{ sm: "left", lg: "center" }}
+                            >
+                                Net price is what you pay after your gift aid is subtracted from the cost of attendance. A lower net price can mean less student debt after graduation. Compare the average net prices of colleges to find your best fit.
+                            </Text>
+                        </Container>
+                        <Search fetchColleges={fetchColleges} />
+                    </>
+  
+    if (type === "explore") {
+        content = search;
+    } else if (type === "saved") {
+        content = 
+            <>
+                <Title order={2} mb="md" ta="center">
+                    Your Saved Colleges
+                </Title>
+                <SavedColleges />
+            </>;
+    } else {
+        content = search;
+    }
 
     return (
         <>
-            <Title order={2} mb="md" ta="center">
-                Find your dream college without breaking the bank
-            </Title>
-            <Container size="md" px="lg">
-                <Text 
-                    color="dimmed" 
-                    size="sm" 
-                    pb="md" 
-                    ta={{ sm: "left", lg: "center" }}
-                >
-                    Net price is what you pay after your gift aid is subtracted from the cost of attendance. A lower net price can mean less student debt after graduation. Compare the average net prices of colleges to find your best fit.
-                </Text>
-            </Container>
-            <Search fetchColleges={fetchColleges} />
+            {content}
         </>
     );
 };
---
+++ src/components/SavedColleges.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { 
+    Table
+} from "@mantine/core";
+import { IconAlertCircle } from '@tabler/icons-react';
+
+export default function SavedColleges() {
+  
+    return (
+        <>
+           <Table 
+              striped 
+              withBorder 
+              withColumnBorders
+          >
+             
+          </Table>
+        </>
+    );
+}
